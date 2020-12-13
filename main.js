6f039fcb00b3493831ed8d79922f085eac03ca3b
Alexsandro T. Nascimento <72424790+Alex-ctba@users.noreply.github.com>
2020-12-13 17:48:45 -0300

    Atualizado

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ var app = document.querySelector('#app')
 var ul = document.querySelector('#app ul')
 
 button.onclick  = function(){
+   var loading = true
+    if(loading === true){
+        let load = document.createElement('p')
+        load.appendChild(document.createTextNode('Carregando...'))
+        load.setAttribute('id', 'load')
+        ul.appendChild(load)
+    }else{
+        document.getElementById('load').remove()
+    }
+
     var user = input.value;
     return new Promise((resolve, reject)=>{
         const api = new XMLHttpRequest('http')
@@ -19,21 +29,32 @@ button.onclick  = function(){
             }
         }
     })
+
     .then((response)=>{
-      for(var key of Object.keys(response)){
-          var li = document.createElement('li')
-          li.style.listStyle = 'none' 
-          li.style.padding = '10px'
-          li.style.backgroundColor = '#f0efeb'
-          li.style.marginTop = '5px'
-          li.style.border = 'solid #fff'
-          li.style.borderRadius = '10px'
-          li.appendChild(document.createTextNode(key +" => "+response[key]))
-          
-          app.appendChild(li)
-      }
-    })
+    
+        
+            for(var key of Object.keys(response)){
+                var li = document.createElement('li')
+                li.style.listStyle = 'none' 
+                li.style.padding = '10px'
+                li.style.backgroundColor = '#f0efeb'
+                li.style.marginTop = '5px'
+                li.style.border = 'solid #fff'
+                li.style.borderRadius = '10px'
+                li.appendChild(document.createTextNode(key +" => "+response[key]))
+                ul.appendChild(li)
+                app.appendChild(ul)
+                input.value = ''
+               
+            }        
+            setTimeout(() => {
+                document.getElementById('load').remove()
+            }, 2000);
+    })   
     .catch((error)=>{
         console.log(error)
-    })
+        document.getElementById('load').remove()
+    }) 
 }
+
+
